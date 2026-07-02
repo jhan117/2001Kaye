ac893a0f5844dedaffebc7671ceca88e9a73363e
kaye-main <jhan117@naver.com>
2026-07-02 16:31:24 +0900

    fix: support empty blog deployment by adding .gitkeep and removing buggy sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,8 @@
 import {defineConfig} from 'astro/config';
 
-import vercel from '@astrojs/vercel/serverless';
-import sitemap from '@astrojs/sitemap';
-
 // https://astro.build/config
 export default defineConfig({
     site: 'https://2001kaye.vercel.app', // 실제 배포될 Vercel 도메인
-    output: 'server',
-    adapter: vercel(),
-    integrations: [sitemap()],
     markdown: {
         shikiConfig: {
             themes: {
